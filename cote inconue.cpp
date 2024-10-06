7b8ecd3e9e0d4b839418349a5a0abf0dae635790
dimitringounouj@gmail.com <dimitringounouj@gmail.com>
2024-10-06 11:02:10 +0200

    premier commit

--- cote inconue.cpp
+++ cote inconue.cpp
@@ -9,6 +9,6 @@ int main ()
     std :: cout <<"Entrer la valeur de l'angle entre les 02 cote du triangle en radiand \n"; cin >> angle;
     c3 = c1*c1 + c2*c2 + 2*c1*c2*cos(angle) ;
     c3 = sqrt(c3) ;
-    std :: cout <<"la valeur du troisimme cote est : "<< c3 <<"\nMercie d'avoir utilise notre programme"
+    std :: cout <<"la valeur du troisimme cote est : "<< c3 <<"\nMercie d'avoir utilise notre programme";
     return 0;
 }
